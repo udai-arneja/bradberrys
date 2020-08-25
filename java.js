bf18074ddddbd78a139f30892328a7ba4c50d346
udai-arneja <61244716+udai-arneja@users.noreply.github.com>
2020-08-25 10:14:51 +0100

    Update java.js

--- java.js
+++ java.js
@@ -2,8 +2,8 @@
   { "use strict"
 
       $(window).on('load', function () {
-        $('#preloader-active').delay(1000).fadeOut('slow');
-        $('body').delay(1000).css({
+        $('#preloader-active').delay(300).fadeOut('slow');
+        $('body').delay(300).css({
           'overflow': 'visible'
         });
       });
